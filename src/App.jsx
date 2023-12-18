8ae3801ca27da83877c88c2d41234b510d0b48c6
harshpathakzz <harshpathak2017@gmail.com>
2023-12-18 19:39:15 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,15 @@ import Home from "./Home";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/:name" element={<MessagePage />} />
-      </Routes>
-    </Router>
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/:name" element={<MessagePage />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
