2d7e7a44c1ae6dc45262a743fef1682c5966065a
harshpathakzz <harshpathak2017@gmail.com>
2023-12-18 22:05:59 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 import MessagePage from "./MessagePage";
 import Home from "./Home";
 import Footer from "./Footer";
@@ -6,10 +7,12 @@ import Footer from "./Footer";
 function App() {
   return (
     <>
+      <Analytics />
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/:name" element={<MessagePage />} />
+          <Route path="*" element={<h1> PAGE NOT FOUND</h1>} />
         </Routes>
       </Router>
       <Footer />
